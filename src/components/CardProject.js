dcd09235887cb18fdd73cbc9f1d057135fb86552
cpnsn <cpoinson@hotmail.fr>
2025-12-21 20:21:04 +0100

    carousel cards mobile height

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -5,7 +5,7 @@ export default function CardProject({ item, index }) {
   return (
     <div className="flex justify-center group my-10">
       <Link className="relative w-[90%]" href={`/projet/${item.id}`}>
-        <div className="w-full bg-white shadow-sm/5 overflow-hidden group-hover:opacity-65 transition-opacity duration-1000 lg:h-[375px] rounded-tr-3xl rounded-bl-3xl p-2">
+        <div className="w-full bg-white shadow-sm/5 overflow-hidden group-hover:opacity-65 transition-opacity duration-1000 h-[375px] rounded-tr-3xl rounded-bl-3xl p-2">
           <div
             className={`rounded-tr-[1.125rem] h-fit pt-[10%] ${
               index % 2 === 0 ? "bg-purple3" : "bg-orange4"
